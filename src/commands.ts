b3a12743a6a1435041e8ada96555764968697f25
JongChan Choi <disjukr@naver.com>
2022-03-21 20:59:17 +0900

    use deno version of pb when available

--- src/commands.ts
+++ src/commands.ts
@@ -1,12 +1,12 @@
-import path from 'path';
 import * as vscode from "vscode";
-import { LanguageClient, ServerOptions, TransportKind } from "vscode-languageclient/node";
+import { LanguageClient } from "vscode-languageclient/node";
 import {
   ENABLEMENT_FLAG,
   LANGUAGE_CLIENT_ID,
   LANGUAGE_CLIENT_NAME,
 } from "./constants";
 import { PbkitExtensionContext } from "./types";
+import { getServerOptions } from "./language-server";
 
 export type Callback = (...args: any[]) => unknown;
 export type Factory = (
@@ -21,30 +21,16 @@ export function startLanguageServer(
 ) {
   return async () => {
     console.log("Start Pbkit language server");
-
     if (extensionContext.client) {
       const client = extensionContext.client;
       extensionContext.client = undefined;
       vscode.commands.executeCommand("setContext", ENABLEMENT_FLAG, false);
       await client.stop();
     }
-    
-    const serverOptions: ServerOptions = {
-      run: {
-        module: context.asAbsolutePath(path.join('dist', '@pbkit', 'pb-cli', 'esm', 'cli', 'pb', 'entrypoint.js')),
-        transport: TransportKind.stdio,
-        args: ["lsp"],
-      },
-      debug: {
-        module: context.asAbsolutePath(path.join('node_modules', '@pbkit', 'pb-cli', 'esm', 'cli', 'pb', 'entrypoint.js')),
-        transport: TransportKind.stdio,
-        args: ["lsp"],
-      }
-    }
     const client = new LanguageClient(
       LANGUAGE_CLIENT_ID,
       LANGUAGE_CLIENT_NAME,
-      serverOptions,
+      await getServerOptions(context),
       extensionContext.clientOptions,
     );
     context.subscriptions.push(client.start());
---
+++ src/language-server.ts
@@ -0,0 +1,55 @@
+import { exec } from "child_process";
+import { platform } from "os";
+import { ExtensionContext } from "vscode";
+import { ServerOptions, TransportKind } from "vscode-languageclient/node";
+
+type GetServerOptionsFn = (context: ExtensionContext) => Promise<ServerOptions>;
+
+export const getServerOptions: GetServerOptionsFn = async (context) => {
+  if (await commandExists("pb")) {
+    return await getDenoServerOptions(context);
+  } else {
+    return await getNodeServerOptions(context);
+  }
+};
+
+export const getDenoServerOptions: GetServerOptionsFn = async () => {
+  return {
+    run: {
+      command: "pb",
+      args: ["lsp"],
+      options: { env: { ...process.env, NO_COLOR: true } },
+    },
+    debug: {
+      command: "pb",
+      args: ["lsp"],
+      options: { env: { ...process.env, NO_COLOR: true } },
+    },
+  };
+};
+
+export const getNodeServerOptions: GetServerOptionsFn = async (context) => {
+  return {
+    run: {
+      module: context.asAbsolutePath(
+        "dist/@pbkit/pb-cli/esm/cli/pb/entrypoint.js",
+      ),
+      transport: TransportKind.stdio,
+      args: ["lsp"],
+    },
+    debug: {
+      module: context.asAbsolutePath(
+        "node_modules/@pbkit/pb-cli/esm/cli/pb/entrypoint.js",
+      ),
+      transport: TransportKind.stdio,
+      args: ["lsp"],
+    },
+  };
+};
+
+export function commandExists(command: string): Promise<boolean> {
+  return new Promise((resolve) => {
+    if (platform() === "win32") resolve(false);
+    else exec(`command -v ${command}`, (error) => resolve(!error));
+  });
+}
